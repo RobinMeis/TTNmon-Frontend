8b79f47b775dc5218aad38a8e4cca652b0b68398
Robin Meis <robin@meis.space>
2018-01-11 00:15:51 +0100

    Use cookies for key management

--- console/pages/getauthkey.js
+++ console/pages/getauthkey.js
@@ -1,7 +1,12 @@
 function getauthkey() {
-  $.post( "https://ttnmon.meis.space/api/token/", function( data ) {
-    auth_key = $.parseJSON( data );
-    $("#auth_key").val(auth_key["auth_token"]);
-  });
+  if (Cookies.get('auth_key') == undefined) { //Create a new key
+    $.post( "https://ttnmon.meis.space/api/token/", function( data ) {
+      auth_key = $.parseJSON( data );
+      $("#auth_key").val(auth_key["auth_token"]);
+      Cookies.set('auth_key', auth_key["auth_token"], { expires: 365, path: '' });
+    });
+  } else //Use existing key
+    $("#auth_key").val(Cookies.get('auth_key'));
+
   $( "#content" ).fadeIn(200);
 }
