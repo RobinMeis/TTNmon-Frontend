570f87cbbdba9b17d43d966ece2b45cabcacdc50
Robin Meis <robin@meis.space>
2018-01-11 00:10:34 +0100

    New page / js handler

--- console/js/menu-load.js
+++ console/js/menu-load.js
@@ -1,5 +1,10 @@
+var json_pages = null;
+
 $(document).ready(function() {
-  parse_url();
+  $.getJSON( "pages/pages.json", function( data ) {
+    json_pages = data;
+    parse_url();
+  });
 });
 
 window.onhashchange = function(){
@@ -7,22 +12,27 @@ window.onhashchange = function(){
 }
 
 function parse_url() {
-
   if (window.location.hash == "" || window.location.hash == "#")
-    load_page("pages/startpage.html");
+    load_page(json_pages["#startpage"]);
   else
-    load_page("pages/" +  window.location.hash.replace("#", "") + ".html");
+    if (json_pages[window.location.hash] != undefined)
+      load_page(json_pages[window.location.hash]);
+    else
+      $( "#content" ).html("Sorry, file not found");
 }
 
 function load_page(file) {
-    var jqxhr = $.get( file, function(data) {
+    $.get( file["html"], function(data) {
       $( "#content" ).fadeOut(200, function() {
         $( "#content" ).html(data);
-        $( "#content" ).fadeIn(299)
-
+        window[file["javascript"]]();
       })
     })
     .fail(function() {
       $( "#content" ).html("Error while loading data. Please try again later");
     })
+}
+
+function defaultpage() {
+  $( "#content" ).fadeIn(200);
 }
