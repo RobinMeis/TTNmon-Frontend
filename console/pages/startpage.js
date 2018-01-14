3d8eddaf69fd5ca21f1ca8e1d552e114ea3beeff
Robin Meis <robin@meis.space>
2018-01-14 21:33:06 +0100

    Updated startpage
    Displaying stats and interisting devices

--- console/pages/startpage.js
+++ console/pages/startpage.js
@@ -1,3 +1,22 @@
 function startpage() {
-  $( "#content" ).fadeIn(200);
+  $.getJSON( "https://ttnmon.meis.space/api/stats/")
+  .done( function( data ) {
+    if (data["error"] == 0) {
+      $("#registered_devices").text(data["stats"]["devices"]["count"]);
+      $("#authorization_tokens").text(data["stats"]["authorizations"]["count"]);
+      $("#received_packets").text(data["stats"]["packets"]["count"]);
+      $("#gateways").text(data["stats"]["gateways"]["count"]);
+    } else {
+      $("#registered_devices").hide();
+      $("#authorization_tokens").hide();
+      $("#received_packets").hide();
+    }
+    $( "#content" ).fadeIn(200);
+  })
+  .fail( function() {
+    $( "#content" ).fadeIn(200);
+    $("#registered_devices").hide();
+    $("#authorization_tokens").hide();
+    $("#received_packets").hide();
+  });
 }
