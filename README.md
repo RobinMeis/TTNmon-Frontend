798731dd10093b2de8230fc2d6fbed7b4b507287
Robin Meis <robin@meis.space>
2018-01-15 20:59:46 +0100

    Graphing

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ HTML Frontend for TTNmon-Backend
 
 Uses SB Admin from https://startbootstrap.com/template-overviews/sb-admin/
 Uses Highcharts
+
+## Highcarts
+This project uses Highcharts.com for graphing. Highcharts is free for non-commercial use. Please make shure not to use TTNmon commercially
--- console/pages/device.js
+++ console/pages/device.js
@@ -14,9 +14,12 @@ function device(hash) {
     $("#date_end").datepicker(options);
 
     var start = new Date(); //Set dates
+    var end = new Date();
     start.setMonth(start.getMonth() - 1);
     $('#date_start').datepicker("setDate", start);
-    $('#date_end').datepicker("setDate", Date());
+    $('#date_end').datepicker("setDate", end);
+    $('#date_end').datepicker("setStartDate", start);
+    $('#date_start').datepicker("setEndDate", end);
 
     $("#date_start").datepicker().on("changeDate", function(e) { //Configure update events
       $('#date_end').datepicker("setStartDate", e.date);
@@ -50,6 +53,21 @@ function get_packets(dev_pseudonym, date_start, date_end) {
       $("#gateways").append('<a class="list-group-item list-group-item-action" href="#"><div class="media"><div class="media-body"><strong>' + gateway["gtw_id"] + '</strong><br>Packets: ' + gateway["packets"] + '<br>RSSI min: ' + gateway["rssi_min"] + '<br>RSSI max: ' + gateway["rssi_max"] + '<br>SNR min: ' + gateway["snr_min"] + '<br>SNR max: ' + gateway["snr_max"] + '<div class="text-muted smaller">' + gateway["lat"] + ' | ' + gateway["lon"] + ' | ' + gateway["alt"] + 'm</div></div></div></a>')
     });
 
+    spreading_factor = []; //Prepare graphs
+  	frequency = [];
+  	packet_count = [];
+  	$.each(data["packets"], function (index, packet) {
+  		date = Date.parse(packet["time"])
+  		spreading_factor.push([date, packet["SF"]]);
+  		frequency.push([date, packet["frequency"]]);
+  		packet_count.push([date, packet["packet_count"]]);
+
+  	});
+
+    graph_frequency(frequency); //Generate graphs
+    graph_spreading_factor(spreading_factor);
+    graph_packet_count(packet_count);
+
     $( "#content" ).fadeIn(200);
   });
 }
@@ -58,4 +76,127 @@ function update_date_button() {
   $( "#content" ).fadeOut(200);
   $("#update_date").hide();
   get_packets(hash[1], $('#date_start').datepicker("getDate"), $('#date_end').datepicker("getDate"), false);
+}
+
+function graph_frequency(data) {
+  Highcharts.chart('chart_frequency', {
+      chart: {
+          zoomType: 'x'
+      },
+      title: {
+          text: null
+      },
+      xAxis: {
+          type: 'datetime'
+      },
+      yAxis: {
+          title: {
+              text: 'Frequency (MHz)'
+          }
+      },
+      legend: {
+          enabled: false
+      },
+      plotOptions: {
+          area: {
+              marker: {
+                  radius: 2
+              },
+              lineWidth: 1,
+              states: {
+                  hover: {
+                      lineWidth: 1
+                  }
+              },
+              threshold: null
+          }
+      },
+
+      series: [{
+          type: 'line',
+          data: data
+      }]
+  });
+}
+
+function graph_spreading_factor(data) {
+  Highcharts.chart('chart_spreading_factor', {
+      chart: {
+          zoomType: 'x'
+      },
+      title: {
+          text: null
+      },
+      xAxis: {
+          type: 'datetime'
+      },
+      yAxis: {
+          title: {
+              text: 'Spreding Factor'
+          }
+      },
+      legend: {
+          enabled: false
+      },
+      plotOptions: {
+          area: {
+              marker: {
+                  radius: 2
+              },
+              lineWidth: 1,
+              states: {
+                  hover: {
+                      lineWidth: 1
+                  }
+              },
+              threshold: null
+          }
+      },
+
+      series: [{
+          type: 'line',
+          data: data
+      }]
+  });
+}
+
+function graph_packet_count(data) {
+  Highcharts.chart('chart_packet_count', {
+      chart: {
+          zoomType: 'x'
+      },
+      title: {
+          text: null
+      },
+      xAxis: {
+          type: 'datetime'
+      },
+      yAxis: {
+          title: {
+              text: 'Packet Count'
+          }
+      },
+      legend: {
+          enabled: false
+      },
+      plotOptions: {
+          area: {
+              marker: {
+                  radius: 2
+              },
+              lineWidth: 1,
+              states: {
+                  hover: {
+                      lineWidth: 1
+                  }
+              },
+              threshold: null
+          }
+      },
+
+      series: [{
+          type: 'area',
+          data: data
+      }]
+  });
 }
