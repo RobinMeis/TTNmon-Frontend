a9d352f7f7ba87d3f2edefeaf7ce7d5ba351233b
Robin Meis <robin@meis.space>
2018-01-29 17:26:18 +0100

    Error handlink + Spinner

--- console/pages/getauthkey.js
+++ console/pages/getauthkey.js
@@ -1,15 +1,21 @@
 function getauthkey() {
   if (Cookies.get('auth_key') == undefined) { //Create a new key
-    $.post( "https://api.ttnmon.meis.space/api/token/", function( data ) {
+    $.post( "https://api.ttnmon.meis.space/api/token/")
+    .done (function( data ) {
       auth_key = $.parseJSON( data );
       $("#auth_key").val(auth_key["auth_token"]);
       Cookies.set('auth_key', auth_key["auth_token"], { expires: 365, path: '' });
       $(".showloggedout").hide();
       $(".showloggedin").show();
-    });
+    })
+    .fail(function() {
+      $("#error_msg").text("Failed to generate a new Authorization Key. Please retry later");
+      $("#error_msg").fadeIn(200);
+    })
   } else //Use existing key
     $("#auth_key").val(Cookies.get('auth_key'));
   $(".reveal").on('click', reveal_pwd);
+  $( "#spinner" ).hide();
   $( "#content" ).fadeIn(200);
 }
 
