92325e85172f68610d316bd348b37d05b5df9d4e
Robin Meis <robin@meis.space>
2018-05-29 22:47:55 +0200

    Gateway List

---
+++ console/pages/gateways.js
@@ -0,0 +1,40 @@
+var table = null;
+
+function gateways() {
+  $.ajax( "https://api.ttnmon.meis.space/api/gateway/list/", {"dataType": 'json', "timeout": 3000})
+
+  .done (function( data ) { //Get table data
+    if (data["error"] == 0) {
+      $.each(data["gateways"], function( key, gateway ) {
+        if (gateway["description"] == null) gateway["description"] = "";
+        $("#devTable tbody").append("<tr data-name=\"" + gateway["gtw_id"] + "\" id=\"" + gateway["gtw_id"] + "\"><td>" + gateway["gtw_id"] + "</td><td>" + gateway["first_seen"] + "</td><td>" + gateway["last_seen"] + "</td><td>" + gateway["packets"] + "</td><td>" + gateway["description"] + "</td><td style=\"text-align:center;\"><a href=\"#device-" + device["pseudonym"] + "\"><i class=\"fa fa-eye\"></i></a></td></tr>");
+      });
+
+      table = $("#devTable").DataTable({ //jQuery DataTables
+        columnDefs: [
+          { targets: [4,5], orderable: false},
+        ],
+        "initComplete": function(settings, json) { //Show table after complete
+          $("#spinner").hide();
+          $( "#content" ).fadeIn(200, function() {
+            table.columns.adjust();
+          });
+        }
+      });
+    } else {
+      $("#error_msg").text(data["msg_en"]);
+      $("#error_msg").show();
+      $("#spinner").hide();
+      $("#content").fadeIn();
+    }
+  })
+
+  .fail(function() {
+    $("#error_msg").text("Failed to retrieve gateways. Please try again later");
+    $("#error_msg").show();
+    $("#device_card").hide();
+    $("#spinner").hide();
+    $("#content").fadeIn();
+    }
+  );
+}
