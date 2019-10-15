8ec4c126100fac91f0041a3ac3ea936e1ad18c7b
RobinMeis <robin.meis@smartnoob.de>
2019-10-15 21:27:33 +0200

    Updated device search (search became unavailable)

--- console/pages/search.js
+++ console/pages/search.js
@@ -7,18 +7,7 @@ $(document).ready(function() {
 function search() {
   var input = $("#search-input").val();
   if (input != "") {
-    $.getJSON( "https://api.beta.ttnmon.meis.space/api/search/?query=" + input, function( result ) {
-      if (result["error"] == 0) { //Show device page
-        window.location.hash = "device-" + result["pseudonym"];
-        $("#search-input").val("");
-      } else { //Not found. Show error
-        window.location.hash = "search";
-        $("#content").fadeOut(200, function() {
-          $("#content").html("<div class=\"container-fluid\"><div class=\"alert alert-info\">Your search query failed. " + result["msg_en"] + "</div></div>");
-          $("#content").fadeIn(200);
-        });
-      }
-    });
+    window.location.hash = "device-" + input;
   }
 }
 
