c70b4ff9800e903a9bb84b75758f7fdb2f57dfeb
RobinMeis <robin.meis@smartnoob.de>
2019-11-27 15:50:54 +0100

    Show gateway data

--- console/pages/device-gateways.js
+++ console/pages/device-gateways.js
@@ -1,7 +1,6 @@
 function device_gateways() {
   $(".gateway_select").click(function(e) { //Gateway button click
     var gtw_id = $(this).attr('id');
-    alert(gtw_id)
     update_hash(hash[0] + "-" + hash[1] + "-" + hash[2] + "-" + hash[3] + "-" + gtw_id, true);
     load_gateway(hash[1], gtw_id, $('#date_start').datepicker("getDate"), $('#date_end').datepicker("getDate"));
   });
@@ -12,187 +11,27 @@ function device_gateways() {
 }
 
 function load_gateway(dev_pseudonym, gtw_id, date_start, date_end) {
-  $("#spinner").show();
   $(".gtw_id").text(gtw_id);
   $("#gtw_hide").attr("href", location.hash);
 
-  var string_start = date_start.getFullYear() + "-" + (date_start.getMonth() + 1) + "-" + date_start.getDate();
-  var string_end = date_end.getFullYear() + "-" + (date_end.getMonth() + 1) + "-" + date_end.getDate() + " 23:59:59";
-
-  $.ajax( "https://api.beta.ttnmon.meis.space/api/gateway/device/?dev_pseudonym=" + dev_pseudonym + "&date_start=" + string_start + "&date_end=" + string_end + "&gtw_id=" + gtw_id + "&timezone_offset=" + get_timeOffset(), {"dataType": 'json', "timeout": 20000})
-    .done(function(data) {
-      var channel = [];
-      var snr_series = [];
-      var rssi_series = []
-
-      $.each(data["packets"], function(index, packet) {
-        date = Date.parse(packet["time"])
-        channel.push([date, packet["channel"]]);
-
-        if (typeof snr_series[packet["channel"]] !== 'undefined') {
-          snr_series[packet["channel"]].push([date, packet["snr"]]);
-        } else {
-          snr_series[packet["channel"]] = [[date, packet["snr"]]];
-        }
-
-        if (typeof rssi_series[packet["channel"]] !== 'undefined') {
-          rssi_series[packet["channel"]].push([date, packet["rssi"]]);
-        } else {
-          rssi_series[packet["channel"]] = [[date, packet["rssi"]]];
-        }
-      });
-
-      graph_gtw_channel(channel);
-      graph_gtw_snr(snr_series);
-      graph_gtw_rssi(rssi_series);
-      $("#spinner").hide();
-      $(".gtw_graph").fadeIn();
-    })
-    .fail(function() {
-      $("#spinner").hide();
-      alert("Request failed");
-    });
+  updateGatewayRSSI(dev_pseudonym, gtw_id, date_start, date_end);
+  updateGatewaySNR(dev_pseudonym, gtw_id, date_start, date_end);
+  updateGatewayChannel(dev_pseudonym, gtw_id, date_start, date_end);
+  $(".gtw_graph").fadeIn();
 }
 
 function hide_gateways() {
   $(".gtw_graph").fadeOut();
 }
 
-function graph_gtw_channel(data) {
-  Highcharts.chart('chart_channel_gtw', {
-      chart: {
-          zoomType: 'x'
-      },
-      title: {
-          text: null
-      },
-      xAxis: {
-          type: 'datetime'
-      },
-      yAxis: {
-          title: {
-              text: 'Channel'
-          }
-      },
-      legend: {
-          enabled: false
-      },
-      plotOptions: {
-          area: {
-              marker: {
-                  radius: 2
-              },
-              lineWidth: 1,
-              states: {
-                  hover: {
-                      lineWidth: 1
-                  }
-              },
-              threshold: null
-          }
-      },
-
-      series: [{
-          type: 'line',
-          data: data
-      }]
-  });
+function updateGatewayRSSI(pseudonym, gtwID, date_start, date_end) {
+  $('#gateway_rssi').attr('src', "https://grafana.ttnmon.meis.space/d-solo/IHcq8HOZk/packet-gateway-metadata?orgId=1&from=" + date_start.getTime() + "&to=" + (date_end.getTime() + 86400000) + "&var-gtwID=" + gtwID + "&var-devPseudonym=" + pseudonym + "&panelId=2");
 }
 
-function graph_gtw_snr(data) {
-  data_series = [];
-  $.each(data, function(index, series) {
-    if (series != undefined) {
-      data_series.push({
-        type: 'line',
-        name: "Channel " + index,
-        data: series
-      });
-    }
-  });
-
-  Highcharts.chart('chart_snr_gtw', {
-      chart: {
-          zoomType: 'x'
-      },
-      title: {
-          text: null
-      },
-      xAxis: {
-          type: 'datetime'
-      },
-      yAxis: {
-          title: {
-              text: 'SNR'
-          }
-      },
-      legend: {
-          enabled: true
-      },
-      plotOptions: {
-          area: {
-              marker: {
-                  radius: 2
-              },
-              lineWidth: 1,
-              states: {
-                  hover: {
-                      lineWidth: 1
-                  }
-              },
-              threshold: null
-          }
-      },
-
-      series: data_series
-  });
+function updateGatewayChannel(pseudonym, gtwID, date_start, date_end) {
+  $('#gateway_channel').attr('src', "https://grafana.ttnmon.meis.space/d-solo/IHcq8HOZk/packet-gateway-metadata?orgId=1&from=" + date_start.getTime() + "&to=" + (date_end.getTime() + 86400000) + "&var-gtwID=" + gtwID + "&var-devPseudonym=" + pseudonym + "&panelId=6");
 }
 
-function graph_gtw_rssi(data) {
-  data_series = [];
-  $.each(data, function(index, series) {
-    if (series != undefined) {
-      data_series.push({
-        type: 'line',
-        name: "Channel " + index,
-        data: series
-      });
-    }
-  });
-
-  Highcharts.chart('chart_rssi_gtw', {
-      chart: {
-          zoomType: 'x'
-      },
-      title: {
-          text: null
-      },
-      xAxis: {
-          type: 'datetime'
-      },
-      yAxis: {
-          title: {
-              text: 'RSSI'
-          }
-      },
-      legend: {
-          enabled: true
-      },
-      plotOptions: {
-          area: {
-              marker: {
-                  radius: 2
-              },
-              lineWidth: 1,
-              states: {
-                  hover: {
-                      lineWidth: 1
-                  }
-              },
-              threshold: null
-          }
-      },
-
-      series: data_series
-  });
+function updateGatewaySNR(pseudonym, gtwID, date_start, date_end) {
+  $('#gateway_snr').attr('src', "https://grafana.ttnmon.meis.space/d-solo/IHcq8HOZk/packet-gateway-metadata?orgId=1&from=" + date_start.getTime() + "&to=" + (date_end.getTime() + 86400000) + "&var-gtwID=" + gtwID + "&var-devPseudonym=" + pseudonym + "&panelId=4");
 }
